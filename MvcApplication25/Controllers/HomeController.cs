c3d21b30506004e49cdf9c6bb26980d616cbb9eb
Avrumi Friedman <info@lakewoodprogramming.com>
2016-05-26 22:34:28 -0400

    Initial commit

---
+++ MvcApplication25/Controllers/HomeController.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using MvcApplication25.Models;
+
+namespace MvcApplication25.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index(string text)
+        {
+            IndexViewModel viewModel = new IndexViewModel();
+            viewModel.Text = text;
+            if (!String.IsNullOrEmpty(text))
+            {
+                viewModel.WordCount = GetWordCount(text);
+            }
+            return View(viewModel);
+        }
+
+
+        private Dictionary<char, int> GetWordCount(string text)
+        {
+            Dictionary<char, int> results = new Dictionary<char, int>();
+            foreach (char c in "ABCDEFGHIJKLMNOPQRSTUVWXYZ")
+            {
+                results.Add(c, 0);
+            }
+
+            foreach (char c in text.ToUpper())
+            {
+                if (results.ContainsKey(c))
+                {
+                    results[c]++;
+                }
+            }
+
+            return results;
+        }
+    }
+}
---
+++ MvcApplication25/Models/IndexViewModel.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace MvcApplication25.Models
+{
+    public class IndexViewModel
+    {
+        public string Text { get; set; }
+        public Dictionary<char, int> WordCount { get; set; } 
+    }
+}
